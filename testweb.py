56a7eee260597d9bbbc0d7d8e70e3772fb82209c
Vishwa rathinam s <95266350+Vishwarathinam@users.noreply.github.com>
2021-12-10 20:17:20 -0800

    Add files via upload

--- testweb.py
+++ testweb.py
@@ -1,26 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-
-To develop a simple webserver to serve html pages.
-## DESIGN STEPS:
-### Step 1:
-
-HTML content creation
-### Step 2:
-
-
-Design of webserver workflow
-### Step 3:
-
-Implementation using Python code
-### Step 4:
-
-Serving the HTML pages.
-### Step 5:
-
-Testing the webserver
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -133,22 +110,3 @@ server_address = ('',2004)
 httpd = HTTPServer(server_address,myhandler)
 print("Website is running...")
 httpd.serve_forever()
-"""
-class myhandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        print("request received")
-        self.send_response(200)
-        self.send_header('content-type', 'text/html; charset=utf-8')
-        self.end_headers()
-        self.wfile.write(content.encode())
-server_address = ('',2004)
-httpd = HTTPServer(server_address,myhandler)
-print("Website is running...")
-httpd.serve_forever()
-
-```
-## OUTPUT:
-![GitHub Logo](unknown.png)
-
-## RESULT:
-A WebServer Has Been Created Successfully!
